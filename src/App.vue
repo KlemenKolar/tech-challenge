b639c71608c8637db0ecbc34f8ad81c249bd6205
KlemenKolar <kk6212@student.uni-lj.si>
2023-07-22 20:32:09 +0200

    template setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>

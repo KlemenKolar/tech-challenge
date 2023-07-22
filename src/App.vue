71ca9f665559e657481c0186f724567db8f7ea19
KlemenKolar <kk6212@student.uni-lj.si>
2023-07-22 20:34:20 +0200

    added navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
+<v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-divider></v-divider>
 
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-account-group" title="Users" value="Users" to="/users"></v-list-item>
+          <v-list-item prepend-icon="mdi-cart-variant" title="Carts" value="Carts" to="/carts"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Products" value="Products" to="/products"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script setup>
